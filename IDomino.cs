e63249b802c19fad1c893df1bc7f0430b3b3698e
el_titi <ovidionp01@gmail.com>
2022-06-07 11:01:31 -0400

    lista de jugadores

--- IDomino.cs
+++ IDomino.cs
@@ -9,21 +9,28 @@ namespace matcom_domino
         //IEnumerable<IFichas<T>> ConjuntoCards {get; } 
         //IEnumerable<IFichas<T>> GeneratedCards();        
         //void TipodeReparticion();
-        List<IFichas<T>> ConjuntodeFichas { get; }
-        void GameOrden();
+               
         void GeneratedCards();
+        List<IFichas<T>> ConjuntodeFichas { get; }
+        List<IPlayer<T>> Jugadores{get;}
+        void AgregarJugador(IPlayer<T> a);
         void RepartirFichas();
+        void GameOrden();
         bool EndGame();
         void Wins();
     }
     public class Referee9 : Domino<int>
     {
+        public List<IFichas<int>> ConjuntodeFichas { get => this.conjuntodeFichas; }
+        private List<IFichas<int>> conjuntodeFichas;
+        public List<IPlayer<int>> Jugadores{get => this.jugadores;}
+        private List<IPlayer<int>>jugadores;
+
         public Referee9()
         {
             this.conjuntodeFichas = new List<IFichas<int>>();
+            this.jugadores=new List<IPlayer<int>>();
         }
-        public List<IFichas<int>> ConjuntodeFichas { get => this.conjuntodeFichas; }
-        private List<IFichas<int>> conjuntodeFichas;
         
         public void GeneratedCards()
         {
@@ -36,6 +43,9 @@ namespace matcom_domino
                     this.conjuntodeFichas.Add(new Fichas9(i, j));
                 }
             }
+        }
+        public void AgregarJugador(IPlayer<int> a){
+            jugadores.Add(a);
         }
         public void RepartirFichas()
         {
