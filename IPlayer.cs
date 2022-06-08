309b98859ffeb1368cfc0c699dc76ec330c43285
JoseCoscu <coscujjose@gmail.com>
2022-06-08 13:15:47 -0400

    Arreglando nombres

--- IPlayer.cs
+++ IPlayer.cs
@@ -3,9 +3,9 @@ namespace matcom_domino
     public interface IPlayer<T>
     {
         //mismo errror que con la lsita de la clase referee del inumeroable
-        //IEnumerable<IFichas<T>> HandCards {get;  set;}
+        
         //cambie el repardor de fichas xq lo tenia puesto en la clase referee pero pienso k el jugador es el k tenga la funcion de coher la fichas
-         //IEnumerable<IFichas<T>> HandCards ();  
+        
          List<IFichas<T>>ManoDeFichas{get;}  
          IFichas <T> SelectCard();
          
