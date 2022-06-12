290f8a3cb0951b1d1bbd67b8a1a032d53d28a6b9
JoseCoscu <coscujjose@gmail.com>
2022-06-12 00:16:44 -0400

    Good game

--- Program.cs
+++ Program.cs
@@ -5,80 +5,74 @@ namespace matcom_domino
 {
     class Program
     {
+        //Muestra la Mesa 
+        public static void MostrarMesa(IMesa<int> Table)
+        {
+            Console.WriteLine("Fichas en mesa");
+            foreach (var ficha in Table.CardinTable)
+            {
+                Console.Write(ficha + ", ");
+            }
+
+            Console.WriteLine();
+        }
+    
+        //Muestra la mano de un jugaddor
+        public static void MostrarMano(IPlayer<int> Player)
+        {
+            Console.WriteLine("Fichas del jugador en turno: "+Player.name);
+            foreach (var ficha in Player.ManoDeFichas)
+            {
+                Console.Write(ficha+", ");
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             // Inicializando Objetos del Juego
-            Domino<int> c = new Referee9();
+            
             Mesa table = new Mesa();
-            IPlayer<int> r = new Player(table);
-            PlayerBotaGorda t = new PlayerBotaGorda(table);
-            PlayerBotaGorda p = new PlayerBotaGorda(table);
-            IPlayer<int> pramd = new PlayerRandom(table);
-            p.SortHand();
-            
+            Domino<int> c = new Referee9(table);
+            IPlayer<int> P1 = new Player(table, "PlayerNormal1");
+            PlayerBotaGorda B1 = new PlayerBotaGorda(table, "PlayerBotaG1");
+            PlayerBotaGorda B2 = new PlayerBotaGorda(table,"PlayerBotaG2");
+            IPlayer<int> R1 = new PlayerRandom(table,"PlayerRandom1");
+
+
             //Agregando Jugadores al Juego
-            c.Jugadores.Add(pramd);
-            c.Jugadores.Add(r);
-            c.Jugadores.Add(t);
-            c.Jugadores.Add(p);
-            
+            c.Jugadores.Add(B1);
+            c.Jugadores.Add(B2);
+            c.Jugadores.Add(R1);
+
             //Generando las Fichas del juego    
             c.GeneratedCards();
-            
+
             // Repartiendo las fichas
             c.RepartirFichas();
 
-            
-            
-            while (true)
-            {
-                foreach (var ficha in pramd.ManoDeFichas)
-                {
-                    Console.Write(ficha+", ");
-                }
 
-                pramd.SelectCard();
-                Console.WriteLine("fichajugable: "+table.fichaJugable);
-                foreach (var ficha in table.CardinTable)
-                {
-                    Console.Write(ficha+", ");
-                }
+            while (!c.EndGame())
+            {
+                MostrarMano(B1);
+                B1.SelectCard();
+                MostrarMesa(table);
+                Console.WriteLine("Ficha Jugable: "+table.fichaJugable);
+                c.Wins();
+                Console.Read();
+                MostrarMano(B2);
+                B2.SelectCard();
+                MostrarMesa(table);
+                Console.WriteLine("Ficha Jugable: "+table.fichaJugable);
+                c.Wins();
+                Console.Read();
+                MostrarMano(R1);
+                R1.SelectCard();
+                MostrarMesa(table);
+                Console.WriteLine("Ficha Jugable: "+table.fichaJugable);
+                Console.Read();
+                c.Wins();
                 
-                Console.WriteLine("Player 1 Jugo!");
-                p.SelectCard();
-                Console.WriteLine("Mesa");
-                Console.WriteLine("fichajugable: "+table.fichaJugable);
-                foreach (var ficha in table.CardinTable)
-                {
-                    Console.Write(ficha+", ");
-                }
-                Console.WriteLine("\n"+"Mano Player 1");
-                foreach (var ficha in p.ManoDeFichas)
-                {
-                    Console.Write(ficha + ", ");
-                }
-                
-                string k = Console.ReadLine();
-                
-                Console.WriteLine("Player 2 Jugo!!");
-                t.SelectCard();
-                Console.WriteLine("fichajugable: "+table.fichaJugable);
-                Console.WriteLine("Mesa");
-                foreach (var ficha in table.CardinTable)
-                {
-                    Console.Write(ficha+", ");
-                }
-                Console.WriteLine("\n"+"Mano Player 2");
-                foreach (var ficha in t.ManoDeFichas)
-                {
-                    Console.Write(ficha + ", ");
-                }
-                
-                k = Console.ReadLine();
-                
-                Console.WriteLine();
-                if (k == "-1")
-                    break;
             }
         }
     }
